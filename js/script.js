3a2158a01819aaecc9d00062f90d92f7cc16aac8
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-03-18 11:58:40 +0100

    Refactor script.js: Comment out unused variables and functions for cleaner code

--- js/script.js
+++ js/script.js
@@ -1,20 +1,52 @@
-var name = "Shafayat";
-let number = 7;
-const isDeveloper = true;
-console.log(name, number, isDeveloper);
-console.log(typeof name);
-console.log(typeof number);
-console.log(typeof isDeveloper);
+// var name = "Shafayat";
+// let number = 7;
+// const isDeveloper = true;
+// console.log(name, number, isDeveloper);
+// console.log(typeof name);
+// console.log(typeof number);
+// console.log(typeof isDeveloper);
 
-var global = "Global";
-function functionForScope() {
-  const functionScope = "function Scope";
-  console.log(functionScope);
-  console.log(global);
-}
+// var global = "Global";
+// function functionForScope() {
+//   const functionScope = "function Scope";
+//   console.log(functionScope);
+//   console.log(global);
+// }
 
-functionForScope();
-if (global) {
-  const name = "This is global Scope";
-  console.log(name);
+// functionForScope();
+// if (global) {
+//   const name = "This is global Scope";
+//   console.log(name);
+// }
+
+const num1 = 7;
+const num2 = 5;
+const sum = num1 + num2;
+const difference = num1 - num2;
+const multiplizieren = num1 * num2;
+const dividieren = num1 / num2;
+const rest = num1 % num2;
+console.log(sum, difference, multiplizieren, dividieren, rest);
+const a = 1;
+const b = 4;
+if (a > 0 && b > 0) {
+  console.log(true);
 }
+if (a > 1 || b > 0) {
+  console.log(true);
+}
+if (a != b) {
+  console.log("a not = b");
+}
+const string = "7";
+const number = 7;
+isTrue = string === number;
+console.log(isTrue);
+isFalse = string == number;
+console.log(isFalse);
+const more = 9 > 7;
+const small = 5 < 7;
+console.log(more, small);
+let i = 7;
+
+console.log(i, (i += 1), (i -= 1), (i *= 2), (i /= 2));
