f785143b8ad81a34ab0ac10b79188773b15a9970
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-03-30 08:23:34 +0200

    home task js

--- js/home.js
+++ js/home.js
@@ -1,40 +1,109 @@
-for (let index = 0; index < 21; index++) {
-  console.log(index);
-}
-for (let index = 0; index < 31; index += 2) {
-  console.log(index);
-}
+// for (let index = 0; index < 21; index++) {
+//   console.log(index);
+// }
+// for (let index = 0; index < 31; index += 2) {
+//   console.log(index);
+// }
 
-for (let i = 0; i < 31; i++) {
-  if (i % 2 !== 0) {
-    console.log(i);
+// for (let i = 0; i < 31; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+// for (let index = 1; index < 51; index++) {
+//   if (index % 3 === 0) {
+//     console.log(index);
+//   }
+// }
+// const number = Number(prompt("bir eded daxil edin"));
+// const quadrat = number ** 2;
+// console.log(quadrat);
+// const result = Number(prompt("balinizi daxil edin"));
+// if (result > 80) {
+//   console.log("ela");
+// } else if (result > 50 && result < 81) {
+//   console.log("kecdi");
+// } else {
+//   console.log("kesildi");
+// }
+// const num = Number(prompt("bir eded daxil edin"));
+// if (num % 2 === 0) {
+//   console.log("eded cutdu");
+// } else {
+//   console.log("eded tekdir");
+// }
+// const enterNumber = Number(prompt("eded daxil edin"));
+// let summe = 0;
+// for (let index = 1; index <= enterNumber; index++) {
+//   summe += index;
+// }
+// console.log(summe);
+
+let i = 1;
+while (i <= 100) {
+  console.log(i);
+
+  if (i % 3 == 0) {
+    continue;
+  }
+  if (i == 50) {
+    break;
   }
 }
-for (let index = 1; index < 51; index++) {
-  if (index % 3 === 0) {
-    console.log(index);
-  }
-}
-const number = Number(prompt("bir eded daxil edin"));
-const quadrat = number ** 2;
-console.log(quadrat);
-const result = Number(prompt("balinizi daxil edin"));
-if (result > 80) {
-  console.log("ela");
-} else if (result > 50 && result < 81) {
-  console.log("kecdi");
-} else {
-  console.log("kesildi");
-}
-const num = Number(prompt("bir eded daxil edin"));
-if (num % 2 === 0) {
-  console.log("eded cutdu");
-} else {
-  console.log("eded tekdir");
-}
-const enterNumber = Number(prompt("eded daxil edin"));
-let summe = 0;
-for (let index = 1; index <= enterNumber; index++) {
-  summe += index;
-}
-console.log(summe);
+// let num = Number(prompt("eded daxil edin:"));
+// let sum = 0;
+// while (num !== 0) {
+//   sum = sum + num;
+// }
+// console.log(sum);
+
+// let cut = 0;
+// let tek = 0;
+// for (let j = 1; j <= 50; j++) {
+//   if (j % 2 == 0) {
+//     cut += j;
+//   } else {
+//     tek += j;
+//   }
+// }
+// console.log("cut ededler" + cut);
+// console.log("tek ededler" + tek);
+
+// for (let ededler = 5; ededler <= 100; ededler += 5) {
+//   console.log(ededler);
+// }
+// let duzgunEded = 7;
+// let daxilEdilenEded = Number(
+//   prompt("duzgun ededi tapanakimi regem eded daxil edin"),
+// );
+// while (daxilEdilenEded !== duzgunEded) {
+//   daxilEdilenEded = alert("yalnis ededdi");
+// }
+
+// let num1 = 1;
+// while (num1) {
+//   console.log(num1);
+//   if (num1 == 20) {
+//     break;
+//   }
+// }
+
+// for (let a = 1; a <= 30; a++) {
+//   if (a % 4 == 0) {
+//     continue;
+//   }
+// }
+
+// let daxilEdilenSoz = prompt("soz daxil edin,dayandirmaq istesez 'stop' yazin");
+// while (daxilEdilenSoz !== "stop") {
+//   daxilEdilenSoz = prompt("stop yaz");
+// }
+// for (let say = 10; say >= 1; say--) {
+//   console.log(say);
+// }
+
+// let cemi = 0;
+// for (let i = 1; i =< 100; i++) {
+//   cemi+= i;
+// }
+// console.log(sum);
