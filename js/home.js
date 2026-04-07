697bcbaa2299fc8f64d21c3f79b3f6d1b57a9bf3
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-04-07 07:53:35 +0200

    homework

--- js/home.js
+++ js/home.js
@@ -110,22 +110,22 @@
 // }
 // console.log(sum);
 
-let number = Number(prompt("Bir rəqəm daxil edin "));
-let result = true;
+// let number = Number(prompt("Bir rəqəm daxil edin "));
+// let result = true;
 
-for (let i = 2; i < Math.floor(number / 2); i++) {
-  if (number % i == 0) {
-    result = false;
-    i++;
-    break;
-  }
-}
+// for (let i = 2; i < Math.floor(number / 2); i++) {
+//   if (number % i == 0) {
+//     result = false;
+//     i++;
+//     break;
+//   }
+// }
 
-if (result) {
-  alert(number + " Prime əddədir ");
-} else {
-  alert(number + " Prime Deyil");
-}
+// if (result) {
+//   alert(number + " Prime əddədir ");
+// } else {
+//   alert(number + " Prime Deyil");
+// }
 
 // 01.04.2026
 
@@ -154,13 +154,13 @@ if (result) {
 // }
 // console.log(isEven(7));
 
-// ?????
 // function findNumber(arr, num) {
 //   for (let index = 0; index < arr.length; index++) {
 //     const element = arr[index];
 //     if (element === num) {
 //       console.log("Found");
 //     }
+//     return;
 //   }
 //   console.log("NotFound");
 // }
@@ -201,3 +201,72 @@ if (result) {
 //   }
 // }
 // console.log(isPositive(-7));
+
+// ----------------------------------------------
+// function isOddOrEven(num) {
+//   if (num % 2 === 0) {
+//     return "num ist even";
+//   } else {
+//     return "num is odd";
+//   }
+// }
+// console.log(isOddOrEven(7));
+
+// function decimalToBinary(number) {
+//   let bin = "";
+//   while (true) {
+//     bin += (number % 2).toString();
+//     number = Math.floor(number / 2);
+
+//     if (number == 1) {
+//       bin += 1;
+//       break;
+//     }
+//   }
+//   console.log(bin);
+// }
+// console.log(decimalToBinary(5));
+
+// function binaryToDecimal(number2) {
+//   return parseInt(number2, 2);
+// }
+// console.log(binaryToDecimal(101));
+
+// function isPerfectNumber(number3) {
+//   let sum = 0;
+//   for (let i = 2; i <= number3 / 2; i++) {
+//     if (number3 % i == 0) {
+//       sum += i;
+//     }
+//   }
+
+//   sum += 1 + number3;
+
+//   if (sum == number3 * 2) {
+//     console.log("perfect number");
+//   } else {
+//     console.log("not perfect number");
+//   }
+// }
+
+// console.log(isPerfectNumber(7));
+// ???????
+// function isArmstrongNumber(number4) {
+//   const stringNumber = number4.toString();
+//   let sum = 0;
+//   for (let index = 0; index < stringNumber.lengt; index++) {
+//     const element = parseInt(stringNumber[index]);
+//     sum += element ** stringNumber.length;
+//   }
+//   return sum === number4;
+// }
+// console.log(isArmstrongNumber(153));
+
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+console.log(factorial(4));
