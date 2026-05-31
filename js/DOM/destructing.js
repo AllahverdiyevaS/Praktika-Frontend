d20d1a1503ef06050c8038815884f876d9930e40
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-05-31 21:33:15 +0200

    feat: add destructuring examples and update script reference in index.html

---
+++ js/DOM/destructing.js
@@ -0,0 +1,53 @@
+// const user = {
+//   name: "Ali",
+//   age: 33,
+// };
+// const { name, age } = user;
+
+// const student = { name: "Ali", course: "Frontend" };
+// const { course } = student;
+// console.log(course);
+// const { name, course, age = 33 } = student;
+// console.log(name, course, age);
+
+// const studentsArray = ["Omar", "Aydin", "Asma", "Asmar", "Jasmin", "Tunar"];
+// const [student1, student2, ...rest] = studentsArray;
+// console.log(student1, student2, rest);
+// const coursesArray = ["HTML", "CSS", "JavaScript"];
+// const [first, , third] = coursesArray;
+// console.log(first, third);
+// const studentsCourses = [...coursesArray, ...studentsArray];
+// console.log(studentsCourses);
+// const array1 = [1, 2, 3];
+// const array2=[...array1]
+const object1 = { name: "Ali", age: 33 };
+// const object2 = { ...object1, course: "Frontend" };
+
+const objekt2 = { ...object1, name: "Asmar" };
+console.log(object1);
+console.log(objekt2);
+
+const object = { name: "Ali", age: 33, course: "Frontend" };
+// for (const key in object) {
+//   console.log(key);
+// }
+// for (const key in object) {
+//   console.log(object[key]);
+// }
+// for (const key in object) {
+//   console.log(`${key}: ${object[key]}`);
+// }
+
+const array = [1, 2, 3, 4, 5];
+for (const value of array) {
+  console.log(value);
+}
+const string = "Salam";
+for (const char of string) {
+  console.log(char);
+}
+let sum = 0;
+for (const value of array) {
+  sum += value;
+}
+console.log(sum);
