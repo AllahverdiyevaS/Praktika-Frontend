327007b32c859e02b337c0223375ad97d1c31a8d
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-06-10 11:09:37 +0200

    feat: update class structures and add new functionalities for Student, Teacher, Product, and User

--- js/DOM/classes.js
+++ js/DOM/classes.js
@@ -1,155 +1,347 @@
-// class Student {
-//   constructor(name, age, score) {
-//     this.name = name;
-//     this.age = age;
-//     this.score = score;
+// // class Student {
+// //   constructor(name, age, score) {
+// //     this.name = name;
+// //     this.age = age;
+// //     this.score = score;
+// //   }
+// // }
+// // const student1 = new Student("Ali", 25, 90);
+// // console.log(student1);
+
+// class Car {
+//   constructor(brand, model, year) {
+//     this.brand = brand;
+//     this.model = model;
+//     this.year = year;
 //   }
 // }
-// const student1 = new Student("Ali", 25, 90);
+// const car1 = new Car("Toyota", "toyota", 2026);
+// console.log(car1);
+
+// class Employeer {
+//   constructor(name, position, salary) {
+//     this.name = name;
+//     this.position = position;
+//     this.salary = salary;
+//   }
+// }
+// const employeer1 = new Employeer("Ali", "Frontend Developer", 5000);
+// console.log(employeer1);
+
+// class Book {
+//   constructor(titel, author, pages) {
+//     this.titel = titel;
+//     this.author = author;
+//     this.pages = pages;
+//   }
+// }
+// const book1 = new Book("Suc ve Ceza", "Fyodor Dostoevsky", 500);
+// console.log(book1);
+// class Phone {
+//   constructor(model, price, color) {
+//     this.model = model;
+//     this.price = price;
+//     this.color = color;
+//   }
+// }
+// const phone1 = new Phone("iPhone 12", 999, "Black");
+// console.log(phone1);
+
+// class Person {
+//   constructor(name, age, gender, height, weight) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.height = height;
+//     this.weight = weight;
+//   }
+//   eat() {
+//     console.log(`${this.name} yemek yeyir`);
+//   }
+//   think() {
+//     console.log(`${this.name} dusunur`);
+//   }
+// }
+// const person1 = new Person("Ali", 30, "Male", 180, 75);
+// console.log(person1);
+// person1.eat();
+// person1.think();
+
+// class BankAccount {
+//   constructor(accountNumber, balance) {
+//     this.accountNumber = accountNumber;
+//     this.balance = balance;
+//   }
+//   deposit(amount) {
+//     this.balance += amount;
+//     console.log(
+//       `Hesaba ${amount} manat elave olundu. Cari balans: ${this.balance} manat.`,
+//     );
+//   }
+// }
+
+// const account1 = new BankAccount("123456789", 700);
+// console.log(account1);
+// account1.deposit(500);
+
+// class Animal {
+//   constructor(name, species, age) {
+//     this.name = name;
+//     this.species = species;
+//     this.age = age;
+//   }
+// }
+// const animal1 = new Animal("Murka", "Cat", 5);
+// console.log(animal1);
+
+// class Student {
+//   constructor(name, age, grade) {
+//     this.name = name;
+//     this.age = age;
+//     this.grade = grade;
+//   }
+// }
+// const student1 = new Student("Ali", 15, "A");
 // console.log(student1);
 
-class Car {
+// class Teacher {
+//   constructor(name, subject, experience) {
+//     this.name = name;
+//     this.subject = subject;
+//     this.experience = experience;
+//   }
+// }
+// const teacher1 = new Teacher("Orhan", "Mathematics", 10);
+// console.log(teacher1);
+
+// class Product {
+//   constructor(name, price, category, description) {
+//     this.name = name;
+//     this.price = price;
+//     this.category = category;
+//     this.description = description;
+//   }
+// }
+// const product1 = new Product("Garnier", 5, "Baxim", "El ucun krem");
+// console.log(product1);
+
+// const valuesLength = Object.values(person1).length;
+// console.log(valuesLength);
+
+// const keys = Object.keys(product1);
+// console.log(keys);
+
+// const teacherValues = Object.values(teacher1).includes("Orhan");
+// console.log(teacherValues);
+
+// class User {
+//   static count = 0;
+//   constructor(name, email, password) {
+//     this.name = name;
+//     this.email = email;
+//     this.password = password;
+//   }
+//   addUser() {
+//     User.count++;
+//   }
+// }
+// const user1 = new User("ALi", "ali@gmail.com", "123456");
+// user1.addUser();
+// console.log(User.count);
+// user1.addUser();
+// console.log(User.count);
+
+// const valuesCar1 = Object.values(car1);
+// console.log(valuesCar1);
+
+// -------------------------------------------
+
+// this,Inheritance,super
+
+// class User {
+//   constructor(name) {
+//     this.name = name;
+//   }
+
+//   writeName() {
+//     console.log(this.name);
+//   }
+// }
+// const user1 = new User("Ali");
+// user1.writeName();
+
+// class Car {
+//   constructor(brand, model) {
+//     this.brand = brand;
+//     this.model = model;
+//   }
+//   showInfo() {
+//     console.log(`Brand:${this.brand}, Model:${this.model}`);
+//   }
+// }
+// const car1 = new Car("Toyota", "toyota");
+// car1.showInfo();
+
+class BankAccount {
+  constructor(balance) {
+    this.balance = balance;
+  }
+  showBalance() {
+    console.log(`Sizin balansiniz: ${this.balance} AZN`);
+  }
+}
+const account1 = new BankAccount(700);
+account1.showBalance();
+
+class Product {
+  constructor(title, price) {
+    this.title = title;
+    this.price = price;
+  }
+  updatePrice(newPrice) {
+    this.price = newPrice;
+    console.log(` ${this.title} ücun yeni qiymeti : ${this.price} AZN`);
+  }
+}
+const product1 = new Product("Süd", 5);
+product1.updatePrice(7);
+
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+// }
+// class Student extends Person {
+//   constructor(firstName, lastName) {
+//     super(firstName, lastName);
+//   }
+//   write() {
+//     console.log(this.firstName, this.lastName);
+//   }
+// }
+// const student1 = new Student("Ali", "Aliyev");
+// student1.write();
+
+class Animal {
+  constructor(age, color) {
+    this.age = age;
+    this.color = color;
+  }
+  showColor() {
+    console.log(` Men ${this.color} rengdeyem`);
+  }
+}
+class Dog extends Animal {
+  constructor(name, age, color) {
+    super(age, color);
+    this.name = name;
+  }
+  showInfo() {
+    console.log(`Adi:${this.name}, Yashi: ${this.age}, Rengi: ${this.color}`);
+  }
+}
+const dog1 = new Dog("Dog1", 5, "grey");
+dog1.showInfo();
+
+class Vehicle {
   constructor(brand, model, year) {
     this.brand = brand;
     this.model = model;
     this.year = year;
   }
+  showInfo() {
+    console.log(this.brand, this.model, this.year);
+  }
 }
-const car1 = new Car("Toyota", "toyota", 2026);
-console.log(car1);
+class Car extends Vehicle {
+  constructor(brand, model, year) {
+    super(brand, model, year);
+  }
+  write() {
+    console.log("Mashin haqqda melumat:");
+    super.showInfo();
+  }
+}
+const car1 = new Car("Toyota", "toyota", 2025);
+car1.write();
 
 class Employeer {
-  constructor(name, position, salary) {
-    this.name = name;
-    this.position = position;
+  constructor(firstName, lastName, salary) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.salary = salary;
   }
+  showInfo() {
+    console.log(this.firstName, this.lastName, this.salary);
+  }
 }
-const employeer1 = new Employeer("Ali", "Frontend Developer", 5000);
-console.log(employeer1);
+class Manager extends Employeer {
+  constructor(firstName, lastName, salary) {
+    super(firstName, lastName, salary);
+  }
+  showInfoForManager() {
+    super.showInfo();
+    console.log("Men Manager  isleyirem ");
+  }
+}
+const manager1 = new Manager("Orkhan", "Aliyev", 5000);
+manager1.showInfoForManager();
+const correctPassworrd = "12345";
+class User {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+  login(password) {
+    password !== correctPassworrd
+      ? console.log("password yalnisdi")
+      : console.log("xos gelmisiz");
+  }
+}
+class Admin extends User {
+  constructor(firstName, lastName, age) {
+    super(firstName, lastName, age);
+  }
+  adminLogin() {
+    super.login("12345");
+    console.log("salam");
+  }
+}
+const admin1 = new Admin("Orkhan", "Aliyev", 38);
+admin1.adminLogin();
 
-class Book {
-  constructor(titel, author, pages) {
-    this.titel = titel;
-    this.author = author;
-    this.pages = pages;
+class Cat extends Animal {
+  constructor(age, color) {
+    super(age, color);
+  }
+  say() {
+    console.log("men pisiyem");
+    super.showColor();
   }
 }
-const book1 = new Book("Suc ve Ceza", "Fyodor Dostoevsky", 500);
-console.log(book1);
-class Phone {
-  constructor(model, price, color) {
-    this.model = model;
-    this.price = price;
-    this.color = color;
-  }
-}
-const phone1 = new Phone("iPhone 12", 999, "Black");
-console.log(phone1);
+const cat1 = new Cat(5, "ag");
+cat1.say();
 
 class Person {
-  constructor(name, age, gender, height, weight) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-    this.height = height;
-    this.weight = weight;
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
   }
-  eat() {
-    console.log(`${this.name} yemek yeyir`);
-  }
-  think() {
-    console.log(`${this.name} dusunur`);
+  introduce() {
+    console.log(`Salam men ${this.firstName} ${this.lastName}`);
   }
 }
-const person1 = new Person("Ali", 30, "Male", 180, 75);
-console.log(person1);
-person1.eat();
-person1.think();
-
-class BankAccount {
-  constructor(accountNumber, balance) {
-    this.accountNumber = accountNumber;
-    this.balance = balance;
+class Student extends Person {
+  constructor(firstName, lastName, group) {
+    super(firstName, lastName);
+    this.group = group;
   }
-  deposit(amount) {
-    this.balance += amount;
-    console.log(
-      `Hesaba ${amount} manat elave olundu. Cari balans: ${this.balance} manat.`,
-    );
+  write() {
+    super.introduce();
+    console.log(`men ${this.group} qrupda oxuyuram`);
   }
 }
-
-const account1 = new BankAccount("123456789", 700);
-console.log(account1);
-account1.deposit(500);
-
-class Animal {
-  constructor(name, species, age) {
-    this.name = name;
-    this.species = species;
-    this.age = age;
-  }
-}
-const animal1 = new Animal("Murka", "Cat", 5);
-console.log(animal1);
-
-class Student {
-  constructor(name, age, grade) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
-}
-const student1 = new Student("Ali", 15, "A");
-console.log(student1);
-
-class Teacher {
-  constructor(name, subject, experience) {
-    this.name = name;
-    this.subject = subject;
-    this.experience = experience;
-  }
-}
-const teacher1 = new Teacher("Orhan", "Mathematics", 10);
-console.log(teacher1);
-
-class Product {
-  constructor(name, price, category, description) {
-    this.name = name;
-    this.price = price;
-    this.category = category;
-    this.description = description;
-  }
-}
-const product1 = new Product("Garnier", 5, "Baxim", "El ucun krem");
-console.log(product1);
-
-const valuesLength = Object.values(person1).length;
-console.log(valuesLength);
-
-const keys = Object.keys(product1);
-console.log(keys);
-
-const teacherValues = Object.values(teacher1).includes("Orhan");
-console.log(teacherValues);
-
-class User {
-  static count = 0;
-  constructor(name, email, password) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
-  addUser() {
-    User.count++;
-  }
-}
-const user1 = new User("ALi", "ali@gmail.com", "123456");
-user1.addUser();
-console.log(User.count);
-user1.addUser();
-console.log(User.count);
-
-const valuesCar1 = Object.values(car1);
-console.log(valuesCar1);
+const student1 = new Student("Ali", "Aliyev", "EFI125A");
+student1.write();
