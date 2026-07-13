5c65910a8680bc08f96002e996c3abd5f475c22c
AllahverdiyevaS <allahverdiyevashafayat@outlook.com>
2026-07-13 15:05:04 +0200

    feat: add currency conversion functionality with HTML structure and JavaScript logic

---
+++ js/DOM/FetchApi/Currency/main.js
@@ -0,0 +1,21 @@
+const amountInput = document.querySelector("#amount");
+const firstCurrency = document.querySelector("#first_currency");
+const secondCurrency = document.querySelector("#second_currency");
+const result = document.querySelector("result");
+const currency = new Currency();
+function runEvenetListener() {
+  amountInput.addEventListener("input", change);
+}
+
+function change() {
+  const amount = Number(amountInput.value.trim());
+  const firstValue =
+    firstCurrency.options[firstCurrency.selectedIndex].textContent;
+  const secondValue =
+    secondCurrency.options[secondCurrency.selectedIndex].textContent;
+  console.log(firstCurrency);
+
+  console.log(firstValue, secondValue);
+
+  currency.change(amount, firstCurrency, secondCurrency);
+}
